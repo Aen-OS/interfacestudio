a91cdae46da0bde57db42fd66ff8f57b1cec72c1
anedjai <46544513+Aen-OS@users.noreply.github.com>
2026-02-14 13:53:06 +0000

    updated projcets tab

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "../ui/hero";
 import About from "../ui/about";
 import ProjectCard from "../ui/project";
 import Skills from "../ui/skills";
+import baLogo from "../public/BA-whitebg.png";
 
 // hero -> about -> work -> testimonials -> cta -> footer
 // blog + services pages later on
@@ -17,13 +18,22 @@ export default function Home() {
           <h1 className="text-6xl md:text-7xl text-center font-manrope font-bold">
             My Work
           </h1>
-          <p className="text-base md:text-lg mt-5 text-center">I like to work with React & Next.JS/Astro but for most front end projects, I'll use Webflow.<br/><br/> Take a look below at my coded front-end projects!</p>
+          <p className="text-base md:text-lg mt-5 text-center">
+            I like to work with React & Next.JS but for most front end projects,
+            I'll use Astro.
+            <br />
+            <br /> Take a look below at my front-end project!
+          </p>
         </div>
         {/* future feature: only projects are shown at first but user can show templates and then filter projects/templates using filters: personal, e-commerce, docs, education, */}
         {/* two buttons: template & project */}
         {/* filter function: portfolio, e-commerce, education, business */}
         <div className="flex-1 flex-col items-center justify-center">
-          <ProjectCard src={null} company="wheel good bike shop" link={null} />
+          <ProjectCard
+            src={baLogo}
+            company="Bloom Academy"
+            link="https://bloomacademy.uk"
+          />
         </div>
       </div>
       <CTA />
--- ui/project.tsx
+++ ui/project.tsx
@@ -1,26 +1,21 @@
+import Image from "next/image";
+
 export default function ProjectCard({ src, company, link }) {
   return (
     <div className="block border-black border-2 p-4 rounded-xl">
-      {/* <img
+      <Image
         alt={company}
         src={src}
-        className="h-56 w-full rounded-tr-3xl rounded-bl-3xl object-cover sm:h-64 lg:h-72 "
-      /> */}
-
-      <p className="text-center w-full h-56">Front Page Coming Soon!</p>
+        className="h-56 w-full rounded-3xl object-cover sm:h-64 lg:h-100"
+      />
 
       <div className="mt-4 flex items-center justify-center gap-4">
         <strong className="font-medium capitalize">{company}</strong>
 
         <span className="block h-px w-8 bg-blue-500"></span>
 
-        <a
-          // href={`https://${link}`}
-          href={null}
-          target="_blank"
-          className="mt-0.5 opacity-75 sm:mt-0 line-through"
-        >
-          Visit Website
+        <a href={link} target="_blank" className="mt-0.5 sm:mt-0">
+          <button className="btn">Visit Website</button>
         </a>
       </div>
     </div>
